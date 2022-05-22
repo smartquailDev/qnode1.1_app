dbb6778f5d537c5e0730b95b946f53555144549b
smartquailDev <smartquail.dev@gmail.com>
2022-05-22 00:39:54 -0500

    celery

--- qnode11_app/qnode11_app/celery.py
+++ qnode11_app/qnode11_app/celery.py
@@ -2,9 +2,9 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnode10_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnode11_app.settings')
 
-app = Celery('qnode10_app')
+app = Celery('qnode11_app')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
